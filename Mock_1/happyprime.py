1d2ca75acc2197b6990696982bc8f008aa803c57
owenstrength <owenstrength@gmail.com>
2024-10-07 17:31:32 -0500

    happyprime

---
+++ Mock_1/happyprime.py
@@ -0,0 +1,33 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def is_prime(n):
+        if n < 2:
+            return False
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+
+@lru_cache(maxsize=None)
+def sum_of_squares(n):
+            return sum([int(i)**2 for i in str(n)])
+
+@lru_cache(maxsize=None)
+def is_happy(n):
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = sum_of_squares(n)
+        return n == 1
+
+@lru_cache(maxsize=None)
+def is_happy_prime(num):
+    return is_prime(num) and is_happy(num)
+
+test_cases = int(input()) 
+
+for _ in range(test_cases):
+    case, num = map(int, input().split())
+    print(f"{case} {num} {'YES' if is_happy_prime(num) else 'NO'}")
+
